da83bb1309bc3386e2f18d984f6bba92787238df
coder7475 <robiulhossain7475@gmail.com>
2024-11-30 11:42:56 +0600

    feat: creating contacts

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,12 @@ import {
 
 import type { LinksFunction } from "@remix-run/node";
 
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
+
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
